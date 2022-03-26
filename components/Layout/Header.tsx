4dfa9ae8b9ec439af901e942ff31d4e01d3c4f44
raj-noua <raj.chhatrala.noua@gmail.com>
2022-03-26 11:47:28 +0530

    Setup banner

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -40,16 +40,34 @@ const CustomLink = ({ item, className }: CustomLinkProps) => {
   );
 };
 
+const text = "We are premium quality bath fittings manufacturers";
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [index, setIndex] = useState(0);
 
   const toggleMenu = useCallback(() => {
     setIsNavOpen((o) => !o);
   }, []);
 
+  useEffect(() => {
+    let direction = 1;
+    let interval = setInterval(() => {
+      setIndex((i) => {
+        if (i <= 0) direction = 1;
+        else if (i === text.length) direction = -1;
+
+        return i + direction;
+      });
+    }, 120);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
-      <header className="bg-primary fixed w-full h-[100px] flex">
+      <header className="bg-primary fixed z-[100] w-full h-[100px] flex">
         <div className="w-[70%] lg:w-[20%] flex items-center ">
           <Image src="/logo.jpg" alt="logo" width="100%" height={50} />
           <p className="text-white text-sm">
@@ -79,7 +97,7 @@ const Header = () => {
 
           {/* Mobile menu start */}
           <div
-            className={`fixed top-0 ${
+            className={`fixed z-[99] top-50 ${
               isNavOpen ? "left-0" : "-left-full"
             } w-screen h-screen bg-secondaryop lg:hidden flex flex-col justify-center items-center text-primary duration-200`}
             onClick={toggleMenu}
@@ -94,7 +112,24 @@ const Header = () => {
           </div>
           {/* Mobile menu end */}
         </div>
+
+        {/* Banner section start */}
+        {/* Banner section end */}
       </header>
+      <div className="h-[100px]"></div>
+      <div className="w-full h-1/2 lg:h-screen relative">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="banner.jpg"
+          alt="banner"
+          className="w-full h-1/2 lg:h-screen object-cover"
+        />
+        <div className="absolute w-full h-full bg-[#000000AA] top-0 left-0 flex justify-center items-center text-center">
+          <span className="text-white text-base px-2 lg:text-4xl font-bold">
+            {text.slice(0, index)}
+          </span>
+        </div>
+      </div>
     </>
   );
 };
